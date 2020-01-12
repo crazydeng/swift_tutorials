71d8ed4b89306001c2e5d2b76879a7482bb4438a
busydeng <ccong.deng@gmail.com>
2020-01-12 17:38:49 +0800

    finish Creating and Combining Views

---
+++ CreatingAndCombiningViews/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 邓焕聪 on 2020/1/12.
+//  Copyright © 2020 crazydeng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height:300)
+            
+            CircleImage()
+                .offset(y:-130)
+                .padding(.bottom,-130)
+            
+            VStack(alignment: .leading){
+                Text("Turtle Rock")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.green)
+                    .multilineTextAlignment(.center)
+                HStack {
+                    Text("Joshua Tree National Park")
+                            .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
