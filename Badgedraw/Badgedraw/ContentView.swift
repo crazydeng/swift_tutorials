ce64aa051b861384013add0008db29bbd7d76187
busydeng <ccong.deng@gmail.com>
2020-01-14 23:34:30 +0800

    update

---
+++ Badgedraw/Badgedraw/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Badgedraw
+//
+//  Created by 邓焕聪 on 2020/1/14.
+//  Copyright © 2020 crazydeng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
